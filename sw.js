44f63bc245dd92ab19c5d364f9b4a4bbaee5c0ff
bryanting <896201664@qq.com>
2023-02-21 14:45:21 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('fetch', function (evt) {
-    console.log('[ServiceWorker] 发送请求', evt.request.url)
+    console.log('[ServiceWorker] 发送请求' + evt.request.url)
 })
 self.addEventListener('activate', function(e) {
     // 浏览器打开网页会会掉此方法，当再次刷新页面不会回调
@@ -8,6 +8,9 @@ self.addEventListener('activate', function(e) {
 self.addEventListener('install', function(e) {
     // 浏览器打开网页会会掉此方法，当再次刷新页面不会回调
     console.log('[ServiceWorker] 已经安装');
+});
+self.addEventListener('error', function(e) {
+    console.log('[ServiceWorker] 错误', e);
 });
 self.addEventListener('notificationclick', function(e) {
     //
