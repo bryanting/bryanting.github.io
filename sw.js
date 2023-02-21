cf4069f21af649e419454511306ce060de356e1c
bryanting <896201664@qq.com>
2023-02-21 11:13:31 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('fetch', function (evt) {
+    console.log('[ServiceWorker] 发送请求', evt.request.url)
+})
+self.addEventListener('activate', function(e) {
+    // 浏览器打开网页会会掉此方法，当再次刷新页面不会回调
+    console.log('[ServiceWorker] 已经被激活');
+});
+self.addEventListener('install', function(e) {
+    // 浏览器打开网页会会掉此方法，当再次刷新页面不会回调
+    console.log('[ServiceWorker] 已经安装');
+});
+self.addEventListener('notificationclick', function(e) {
+    //
+    console.log('[ServiceWorker] 通知被点击');
+});
+self.addEventListener('notificationclose', function(e) {
+    // 通知被点击
+    console.log('[ServiceWorker] 通知被关闭');
+});
